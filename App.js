02060eed854d8b6cfaa6a68e9069b9fdc5f7ffec
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-09-19 16:00:53 -0600

    proyecto iniciado con react native y kitten UI

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import * as eva from "@eva-design/eva";
+import React from "react";
+import icon from "./assets/icon.png";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+const HomeScreen = () => (
+	<Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+		<Text category="h1">HOME</Text>
+	</Layout>
+);
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<ApplicationProvider {...eva} theme={eva.light}>
+			<HomeScreen />
+		</ApplicationProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
